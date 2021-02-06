29ef426bdd12bd8fde80448fee537c059a1dafae
Luis <l.oliveira@kigroup.de>
2021-02-06 16:11:55 +0000

    exercise 4 done

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,42 +2,9 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
-
+function Board({squares, selectSquare}) {
   function renderSquare(i) {
     return (
       <button className="square" onClick={() => selectSquare(i)}>
@@ -48,8 +15,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,21 +30,75 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
+const INITIAL_SQUARES_STATE = Array(9).fill(null)
+
 function Game() {
+  const [squares, setSquares] = useLocalStorageState(
+    'squares',
+    INITIAL_SQUARES_STATE,
+  )
+  const [history, setHistory] = useLocalStorageState('history', [])
+
+  const currentStep = calculateCurrentStep(squares)
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  React.useEffect(() => {
+    if (history.length <= squares.filter(square => square !== null).length) {
+      setHistory([...history, [currentStep, squares]])
+    }
+  }, [squares])
+
+  function selectSquare(square) {
+    if (winner || squares[square]) {
+      return
+    }
+
+    let squaresCopy = [...squares]
+    squaresCopy[square] = nextValue
+    setSquares(squaresCopy)
+  }
+
+  function restart() {
+    setSquares(INITIAL_SQUARES_STATE)
+    setHistory([])
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={squares} selectSquare={selectSquare} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>
+          {history.map(elem => (
+            <li key={elem[0]}>
+              <button
+                onClick={() => setSquares(elem[1])}
+                disabled={elem[0] === currentStep}
+              >
+                {elem[0] === 0 ? 'Go to game start' : `Go to move#${elem[0]}`}
+                {elem[0] === currentStep && '(current)'}
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   )
+}
+
+function calculateCurrentStep(squares) {
+  return squares.filter(square => square !== null).length
 }
 
 // eslint-disable-next-line no-unused-vars
